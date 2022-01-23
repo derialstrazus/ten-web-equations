6bc5cbfe359aa6806357dd12406161541ad1d095
Derek <lee.derek@bcg.com>
2022-01-23 16:01:20 -0600

    init

---
+++ ten-web-equations-backend/ten-web-equations-backend/Controllers/EquationController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ten_web_equations_backend.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EquationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public int Get()
+        {
+            return 3;
+
+            throw new NotImplementedException();
+        }
+    }
+}
